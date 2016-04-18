5f9f9486e9e0195c90f43c04c18536f3f6148676
Michał Zieliński <michal@zielinscy.org.pl>
2016-04-18 19:20:29 +0200

    bytes: add string.setAt

--- collections/bytes.nim
+++ collections/bytes.nim
@@ -8,3 +8,7 @@ proc toBinaryString*[T: array](s: T): string =
   const size = s.high - s.low + 1
   result = newString(size)
   copyMem(result.cstring, unsafeAddr(s), size)
+
+proc setAt*(s: var string, at: int, data: string) =
+  doAssert(s.len >= data.len + at)
+  copyMem(cast[pointer](cast[int](addr s[0]) + at), data.cstring, data.len)
