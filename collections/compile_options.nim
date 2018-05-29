d681f8891698549d5e8448524975d8f6a9824ca7
Michał Zieliński <michal@zielinscy.org.pl>
2018-05-29 13:17:24 +0200

    compile_options file for inclusion from NimScript

---
+++ collections/compile_options.nim
@@ -0,0 +1,21 @@
+# A set of recommended compile options.
+
+when not defined(nimscript):
+  error("this module should only be used from nimscript")
+
+when defined(sanitizer):
+  switch("passC", "-fsanitize=undefined -fsanitize=address")
+  switch("passL", "-fsanitize=undefined -fsanitize=address")
+  switch("cc", "clang")
+
+switch("threads", "on")
+switch("passC", "-g")
+switch("passL", "-g")
+
+switch("verbosity", "1")
+switch("hint.ConvFromXtoItselfNotNeeded", "off")
+switch("hint.XDeclaredButNotUsed", "off")
+
+switch("obj_checks", "on")
+switch("field_checks", "on")
+switch("bound_checks", "on")
